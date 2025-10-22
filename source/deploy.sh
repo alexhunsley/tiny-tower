357c561176bc160c3b34929cee71bf2296235eb8
Alex Hunsley <alex.hunsley@gmail.com>
2025-10-22 10:39:35 +0100

    Tweak scripts

--- source/deploy.sh
+++ source/deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 echo
-echo "Packing SPA...\n"
+echo "Packing SPA..."
+echo
 
 python pack_single.py
 
 mv tower.html ../../../blog/playing_and_noticing/static/
 
+echo
 echo "Deployed tower.html to blog static/"
+echo
 echo
